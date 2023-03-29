060e1749bb5ce25ccddefecc4a52eec8fcdd3021
cjones1047 <cjmj1047@gmail.com>
2023-03-29 06:56:30 -0500

    user_input converted to morse code using for loop and morse_code_dict

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+morse_code_dict = {
+    'A': '.-', 'B': '-...',
+    'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-',
+    'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-',
+    'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--',
+    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----', ',': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-'
+}
+
 user_input = input('Please enter phrase to convert to morse code:\n')
+user_input_list = list(user_input.upper())
+user_input_morse = ''
 
-user_input_morse = user_input
+for char in user_input_list:
+    if char == ' ':
+        user_input_morse += ' '*5
+        continue
+    try:
+        user_input_morse += morse_code_dict[char] + ' '
+    except KeyError:
+        print(f'Removing "{char}" from input...')
 
+print()
 print('Your phrase in morse code:')
 print(user_input_morse)
